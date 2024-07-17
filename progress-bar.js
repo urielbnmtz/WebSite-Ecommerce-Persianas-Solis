7ffd415c86f56b49d1e0e9199d6d488bee927766
Irvin Uriel Blanco Martinez <iubn7mtz@gmail.com>
2024-07-16 20:18:18 -0600

    finalizado el diseño css de los input range (altoXancho), queda pendiente la implementacion del gradient-color en los navegadores --webkit

--- progress-bar.js
+++ progress-bar.js
@@ -1,14 +1,8 @@
-const startValue = 60;
-const endValue = 300;
-const numberOfValues = 25;
+const slideValue = document.querySelector(".sliderValue-span.p1");
+const inputSlider = document.querySelector(".field-input.p1");
 
-const mappedValues = mapValues(startValue, endValue, numberOfValues);
-
-const slideValue = document.querySelector(".sliderValue-span");
-const inputSlider = document.querySelector(".field-input");
-
-const BtnMinVal = document.querySelector(".value-left");
-const BtnMaxVal = document.querySelector(".value-right");
+const BtnMinVal = document.querySelector(".value-left.p1");
+const BtnMaxVal = document.querySelector(".value-right.p1");
 
 
 
@@ -24,6 +18,11 @@ function mapValues(start, end, numValues) {
 }
 
 inputSlider.oninput = (() => {
+    let startValue = 60;
+    let endValue = 300;
+    let numberOfValues = 25;
+    let mappedValues = mapValues(startValue, endValue, numberOfValues);
+
     let value = inputSlider.value;
     console.log(value);
     let progress = (inputSlider.value / 25) * 100;
@@ -40,5 +39,40 @@ inputSlider.oninput = (() => {
     } else if (value == 1){
         slideValue.classList.remove("show"); 
         BtnMinVal.classList.add("choose");
+    }
+});
+
+
+const slideValue2 = document.querySelector(".sliderValue-span.p2");
+const inputSlider2 = document.querySelector(".field-input.p2");
+
+const BtnMinVal2 = document.querySelector(".value-left.p2");
+const BtnMaxVal2 = document.querySelector(".value-right.p2");
+
+
+
+
+inputSlider2.oninput = (() => {
+    let startValue = 60;
+    let endValue = 400;
+    let numberOfValues = 35;
+    let mappedValues = mapValues(startValue, endValue, numberOfValues);
+
+    let value = inputSlider2.value;
+    console.log(value);
+    let progress = (inputSlider2.value / 35) * 100;
+
+    slideValue2.textContent =  mappedValues[value - 1] + "cm";
+    slideValue2.style.left = progress + "%";
+    slideValue2.classList.add("show"); 
+    BtnMinVal2.classList.remove("choose");
+    BtnMaxVal2.classList.remove("choose");
+
+    if (value == 35) {
+        slideValue2.classList.remove("show"); 
+        BtnMaxVal2.classList.add("choose");
+    } else if (value == 1){
+        slideValue2.classList.remove("show"); 
+        BtnMinVal2.classList.add("choose");
     }
 });
